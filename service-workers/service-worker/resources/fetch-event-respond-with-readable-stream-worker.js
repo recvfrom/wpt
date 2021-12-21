014a0589c608de40f6884a470deeca248772d42b
Yutaka Hirano <yhirano@chromium.org>
2021-12-20 23:50:48 -0800

    Stream cancellation in page should be notified to service worker
    When the script transfers a body stream via FetchEvent.RespondWith and
    the stream consumption is cancelled in page, that cancellation should be
    observable in the service worker via UnderlyingSource.cancel.
    
    Bug: 638494
    Change-Id: Ib46c9ae73c17dd2c04432d23b06dad76466105ed

--- service-workers/service-worker/resources/fetch-event-respond-with-readable-stream-worker.js
+++ service-workers/service-worker/resources/fetch-event-respond-with-readable-stream-worker.js
@@ -1,9 +1,38 @@
 'use strict';
 importScripts("/resources/testharness.js");
 
+const map = new Map();
+
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   if (!url.searchParams.has('stream')) return;
+
+  if (url.searchParams.has('observe-cancel')) {
+    const id = url.searchParams.get('id');
+    if (id === undefined) {
+      event.respondWith(new Error('error'));
+      return;
+    }
+    map.set(id, 'pending');
+
+    const stream = new ReadableStream({
+      cancel() {
+        map.set(id, 'cancelled');
+      }
+    });
+    event.respondWith(new Response(stream));
+    return;
+  }
+
+  if (url.searchParams.has('query-cancel')) {
+    const id = url.searchParams.get('id');
+    if (id === undefined) {
+      event.respondWith(new Error('error'));
+      return;
+    }
+    event.respondWith(new Response(map.get(id)));
+    return;
+  }
 
   if (url.searchParams.has('use-fetch-stream')) {
     event.respondWith(async function() {
